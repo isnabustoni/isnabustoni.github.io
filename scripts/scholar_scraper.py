6b4d0ef93888d73b001851d5b47e69f4f1616581
isnabustoni <isna.bustoni@gmail.com>
2026-03-16 15:08:09 +0100

    Create scholar_scraper.py

---
+++ scripts/scholar_scraper.py
@@ -0,0 +1,26 @@
+from scholarly import scholarly
+import json
+
+# Replace with your Google Scholar ID
+SCHOLAR_ID = "YOUR_GOOGLE_SCHOLAR_ID"
+
+author = scholarly.search_author_id(SCHOLAR_ID)
+author = scholarly.fill(author)
+
+publications = []
+
+for pub in author['publications']:
+    pub_filled = scholarly.fill(pub)
+
+    publications.append({
+        "title": pub_filled['bib'].get('title',""),
+        "authors": pub_filled['bib'].get('author',""),
+        "year": pub_filled['bib'].get('pub_year',""),
+        "venue": pub_filled['bib'].get('venue',""),
+        "link": pub_filled.get('pub_url',"")
+    })
+
+with open("data/publications.json","w") as f:
+    json.dump(publications,f,indent=2)
+
+print("Publications updated.")
